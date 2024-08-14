0484276d3f6c68da5b4720ef7e31877cbb4573b7
David <soydoft@gmail.com>
2024-08-13 21:42:41 -0400

    add cors, add express, fix header links, fix backend quiz endpoint

--- backend/server.js
+++ backend/server.js
@@ -1,40 +1,68 @@
 const express = require('express');
-const mysql = require('mysql2');
+const cors = require('cors');
 const path = require('path');
 const OpenAI = require('openai');
-const client = new OpenAI({apiKey: process.env.OPENAI_KEY})
+const client = new OpenAI({apiKey: process.env.OPENAI_KEY || 'sk-24XRUcKS9ho9U8yoQDhUjrrrM-5PVUUt_D6qD_80j3T3BlbkFJ_0w0YSjNevPH69NxIuD3FxIdOh8pEvvMWfJT-EhcoA'})
 
 const app = express();
 const router = express.Router();
 app.use('/api', router);
+app.use(express.static(path.join(__dirname, '/dist')));
+app.use(cors());
 
 const PORT = process.env.PORT || 8000;
 
-app.get('/quiz', async (req, res) => {
+router.get('/quiz', async (req, res) => {
     const { topic, level, questionNum, style } = req.query;
-
-    const chatCompletion = chatGPT(
-    [{
-        role: "system",
-        content: `
-        Create a ${questionNum} question quiz in the style of ${style}
-        and in a difficulty level of ${level} regarding ${topic} following
-        the format shown below inside the elipses.
-
-        ...
-        1. Generated question 1.
-        2. Generated question 2.
-        n. Generated question n.
-        ...
-
-        Ensure it is obvious that you are ${style} and that a(n) ${level} could answer the question.
-        `
-    }]);
-
-    res.json(chatCompletion.choices[0].message.content)
+    try {
+        const chatCompletion = chatGPT(
+            [{
+                role: "system",
+                content: `
+                Create a ${questionNum} question quiz in the style of ${style}
+                and in a difficulty level of ${level} regarding ${topic} following
+                the format shown below inside the elipses.
+                
+                ...
+                1. Generated question 1.
+                2. Generated question 2.
+                n. Generated question n.
+                ...
+                
+                Ensure it is obvious that you are ${style} and that a(n) ${level} could answer the question.
+                `
+            }]);
+            console.log(chatCompletion);
+            console.log('OUTSIDE');
+            res.json(chatCompletion.choices[0].message.content)
+    } catch (err) {
+        console.log(err, 'Error: Invalid response');
+        res.json({
+            "id": "chatcmpl-abc123",
+            "object": "chat.completion",
+            "created": 1677858242,
+            "model": "gpt-4o-mini",
+            "usage": {
+                "prompt_tokens": 13,
+                "completion_tokens": 7,
+                "total_tokens": 20
+            },
+            "choices": [
+                {
+                    "message": {
+                        "role": "assistant",
+                        "content": "\n\nThis is a test!"
+                    },
+                    "logprobs": null,
+                    "finish_reason": "stop",
+                    "index": 0
+                }
+            ]
+        })
+    }
 })
 
-app.get('/results', async (req, res) => {
+router.get('/results', async (req, res) => {
     const { question, style, level } = req.query;
     let chatCompletion = {};
     try {
@@ -58,20 +86,19 @@ app.get('/results', async (req, res) => {
     } catch (err) {
         console.log(err, 'Error: Invalid response');
     }
-    
     const result = chatCompletion.choices[0].message.content;
-
-    if (result.substring(0,3).toLowerCase() === "yes") res.json({ verdict: "Correct", explanation: result});
-    else res.json({ verdict: "Incorrect", explanation: result});
+    res.json(result);
 })
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '/dist/', 'index.html'));
+    // res.sendFile(path.join(__dirname, '/dist/', 'index.html'));
+    console.log('*')
+    res.send('hurr');
 });
 
 async function chatGPT(prompt) {
     return await client.chat.completions.create({
-        model: 'gpt-4',
+        model: 'gpt-4o-mini',
         messages: prompt,
         stream: true,
         temperature: 1,
